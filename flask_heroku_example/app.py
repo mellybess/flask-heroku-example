bf1847aad0500a1f45e26b73f0c3e72ab1504ee4
Melissa Smith <m.bess.smith@gmail.com>
2019-09-28 15:22:26 -0700

    initial commit

---
+++ flask_heroku_example/app.py
@@ -0,0 +1,29 @@
+import os
+import psycopg2
+from flask import Flask, render_template, g
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+
+
+def connect_db():
+    return psycopg2.connect(os.environ.get('DATABASE_URL'))
+
+
+@app.before_request
+def before_request():
+    g.db_conn = connect_db()
+
+
+@app.route('/')
+def index():
+    cur = g.db_conn.cursor()
+    cur.execute("SELECT * FROM country;")
+    return render_template('index.html', countries=cur.fetchall())
+
+@app.route('/author/<id>')
+def author(id):
+    cur = g.db_conn.cursor()
+    author = cur.execute("SELECT * FROM author WHERE id=%s;", (id,))
+    return render_template('author.html', author_name=cur.fetchone())
